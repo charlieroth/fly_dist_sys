32cfa73e50dbdb89a4bc0c38ebb663960d8c6e28
charlieroth <charlieroth4@icloud.com>
2023-04-18 15:08:49 +0200

    moduledoc for Node

--- lib/maelstrom/node.ex
+++ lib/maelstrom/node.ex
@@ -1,4 +1,10 @@
 defmodule Maelstrom.Node do
+  @moduledoc """
+  Represents a maelstrom Node
+
+  Handles incoming messages and sending returning appropriate response structs
+  """
+
   use Agent
   alias Maelstrom.Message
   alias Maelstrom.Message.{Init, InitOk, Echo, EchoOk, Generate, GenerateOk}
@@ -11,48 +17,54 @@ defmodule Maelstrom.Node do
     Agent.get(node, fn state -> state end)
   end
 
-  def handle(node, data) do
-    case Maelstrom.parse(data) do
-      %Message{body: %Init.Body{}} = msg ->
-        Agent.get_and_update(node, fn state ->
-          body = msg |> handle_message()
-
-          {%Message{src: msg.body.node_id, dest: msg.src, body: body},
-           %{state | id: msg.body.node_id, node_ids: msg.body.node_ids}}
-        end)
-
-      %Message{body: %Echo.Body{}} = msg ->
-        Agent.get_and_update(node, fn state ->
-          body = msg |> handle_message()
-          {%Message{src: state.id, dest: msg.src, body: body}, state}
-        end)
-
-      %Message{body: %Generate.Body{}} = msg ->
-        Agent.get_and_update(node, fn state ->
-          body = msg |> handle_message()
-          {%Message{src: state.id, dest: msg.src, body: body}, state}
-        end)
-    end
+  def handle(node, msg) do
+    msg |> Maelstrom.parse() |> handle_message(node)
   end
 
-  def handle_message(%Message{body: %Init.Body{}} = msg) do
-    %InitOk.Body{
-      in_reply_to: msg.body.msg_id
-    }
+  def handle_message(%Message{body: %Init.Body{}} = msg, node) do
+    Agent.get_and_update(node, fn state ->
+      response = %Message{
+        src: msg.body.node_id,
+        dest: msg.src,
+        body: %InitOk.Body{
+          in_reply_to: msg.body.msg_id
+        }
+      }
+
+      new_state = %{state | id: msg.body.node_id, node_ids: msg.body.node_ids}
+
+      {response, new_state}
+    end)
   end
 
-  def handle_message(%Message{body: %Echo.Body{}} = msg) do
-    %EchoOk.Body{
-      msg_id: msg.body.msg_id,
-      in_reply_to: msg.body.msg_id,
-      echo: msg.body.echo
-    }
+  def handle_message(%Message{body: %Echo.Body{}} = msg, node) do
+    Agent.get_and_update(node, fn state ->
+      response = %Message{
+        src: state.id,
+        dest: msg.src,
+        body: %EchoOk.Body{
+          msg_id: msg.body.msg_id,
+          in_reply_to: msg.body.msg_id,
+          echo: msg.body.echo
+        }
+      }
+
+      {response, state}
+    end)
   end
 
-  def handle_message(%Message{body: %Generate.Body{}} = msg) do
-    %GenerateOk.Body{
-      id: UUID.uuid4(),
-      in_reply_to: msg.body.msg_id
-    }
+  def handle_message(%Message{body: %Generate.Body{}} = msg, node) do
+    Agent.get_and_update(node, fn state ->
+      response = %Message{
+        src: state.id,
+        dest: msg.src,
+        body: %GenerateOk.Body{
+          id: UUID.uuid4(),
+          in_reply_to: msg.body.msg_id
+        }
+      }
+
+      {response, state}
+    end)
   end
 end
