bb48e92fe84bdf1095bce7b55cb75ce340daeef4
charlieroth <charlieroth4@icloud.com>
2023-04-18 15:07:27 +0200

    typespec for Message struct

--- lib/maelstrom/message/message.ex
+++ lib/maelstrom/message/message.ex
@@ -1,5 +1,15 @@
 defmodule Maelstrom.Message do
+  @moduledoc """
+  A Maelstrom Protocol Message
+
+  https://github.com/jepsen-io/maelstrom/blob/main/doc/protocol.md
+  """
   @derive Jason.Encoder
   @enforce_keys [:src, :dest, :body]
+  @type t :: %{
+          src: String.t(),
+          dest: String.t(),
+          body: map()
+        }
   defstruct [:src, :dest, :body]
 end
